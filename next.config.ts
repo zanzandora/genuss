168884f42243f30d337e4b55f66429c65b1c12c1
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-11-03 13:48:42 +0700

    initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* config options here */
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
