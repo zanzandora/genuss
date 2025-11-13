84c88319a912aae0b4c56e959385bcc91d4e09d9
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-11-14 00:58:14 +0700

    fix(vercel-deploy) :Update room detail page route type to include locale parameter

--- src/app/[locale]/(maketing)/room-detail/[slug]/page.tsx
+++ src/app/[locale]/(maketing)/room-detail/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import NormalBanner from '@/components/sections/(maketing)/banner/NomalBanner';
 import RoomDetailsContent from '@/components/sections/(maketing)/room/content/RoomDetailsContent';
 
-const RoomDetailsPage = async (props: PageProps<'/room-detail/[slug]'>) => {
+const RoomDetailsPage = async (
+  props: PageProps<'/[locale]/room-detail/[slug]'>,
+) => {
   const { slug } = await props.params;
 
   return (
