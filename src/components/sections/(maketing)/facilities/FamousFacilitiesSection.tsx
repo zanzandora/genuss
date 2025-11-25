a6d2f7c94655c7aa89f2fc6e5ba60b8d8c99c077
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-11-26 00:05:59 +0700

    refactor: remove en.d.json.ts from tracking git

--- src/components/sections/(maketing)/facilities/FamousFacilitiesSection.tsx
+++ src/components/sections/(maketing)/facilities/FamousFacilitiesSection.tsx
@@ -1,3 +1,4 @@
+import AnimatedContainer from '@/components/ui/animations/AnimatedContainer';
 import AnimatedSection from '@/components/ui/animations/AnimatedSection';
 import { BLUR_DATA_URL } from '@/constants';
 import { Link } from '@/i18n/routing';
